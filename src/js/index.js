b1fbb3eade974ce2635f04270a6241f3cf8c2433
Ben Lee <leeohsunshine@gmail.com>
2016-03-15 13:14:04 -0400

    Finished

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,14 @@
 // Javascript Entry Point
+import $ from 'jquery';
+
+var urlOfAPI = "https://api.github.com/users/wycats/orgs"
+
+$.ajax(urlOfAPI).then(function (dataLoaded){
+	dataLoaded.forEach(function({login, avatar_url}){
+		$(".app").append(`<div class="org_slot">
+							<img class="avatar" src="${avatar_url}"" alt="${login} logo">
+							<span>${login}</span>
+						  </div>
+			`)
+	})
+})
